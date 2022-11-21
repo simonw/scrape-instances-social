ea5222609da3c041715b64d8864c80dca176cba6
Simon Willison <swillison@gmail.com>
2022-11-21 10:23:50 -0800

    Filter out some instances that threw off the numbers, refs #4

--- build-count-history.sh
+++ build-count-history.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 git-history file counts.db instances.json \
-  --convert "return [
+  --convert "
+  bad_instances = [
+    'angelfire.glitch.me',
+    'angelfire-1.glitch.me',
+    'angelfire-2.glitch.me',
+    'angelfire-3.glitch.me',
+    'you-think-your-fake-numbers-are-impressive.well-this-instance-contains-all-living-humans.lubar.me',
+    'gab.ai',
+    'gab.com',
+    'kathrynmorton21@mastodon.social',
+    'banco@mastodon.social',
+    'zahra@mastodon.social',
+    'junhong@mastodon.social',
+  ]
+  instances = [
+      instance for instance in json.loads(content)
+      if instance['name'] not in bad_instances
+  ]
+  return [
     {
         'id': 'all',
-        'users': sum(d['users'] or 0 for d in json.loads(content)),
-        'statuses': sum(int(d['statuses'] or 0) for d in json.loads(content)),
+        'users': sum(d['users'] or 0 for d in instances),
+        'statuses': sum(int(d['statuses'] or 0) for d in instances),
     }
   ]" --id id
